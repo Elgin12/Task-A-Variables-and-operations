5e43bece87cbdd220101474b73e015b6c8e812ef
Elgin12 <47749193+Elgin12@users.noreply.github.com>
2026-01-17 20:34:27 +0100

    Refactor number reversal logic in Exercise A.04.cpp

--- Exercise A.04.cpp
+++ Exercise A.04.cpp
@@ -2,18 +2,16 @@
 #include <stdio.h>
 
 int main() {
-    int n, rev = 0;
+    int n, rev;
 
     printf("Enter a number (1 to 3 digits): ");
     scanf("%d", &n);
 
-    while (n > 0) {
-        rev = rev * 10 + (n % 10);
-        n /= 10;
-    }
+    rev = (n % 10) * 100 + ((n / 10) % 10) * 10 + (n / 100);
+    rev /= (rev % 100 == 0) ? 100 : (rev % 10 == 0) ? 10 : 1;
 
     printf("Reversed number: %d\n", rev);
-
     return 0;
 }
+
 
